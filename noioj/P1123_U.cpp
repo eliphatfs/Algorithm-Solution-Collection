5bfe3242269bfd56f898933e22a057c523436e55
Obsidianss <shiruoxi2010@163.com>
2017-09-23 18:44:48 +0800

    NOI官方题库1123，未解决
    70分WA，原因不明。三个数据点比正确值大1。

---
+++ noioj/P1123_U.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int data[200000];
+int w[200000];
+int dataraw[200000];
+int main()
+{
+    int n,c;
+    cin>>n>>c;
+    for (int i=0; i<n; i++)
+    {
+        scanf("%d", &dataraw[i]);
+    }
+    sort(dataraw, dataraw + n);
+    int lst;
+    int dpt = 0, cntw = 0;
+    for (int i=0; i<n; i++)
+    {
+        if (i==0 || lst != dataraw[i])
+        {
+            if (i != 0)
+                w[dpt-1] = cntw;
+            data[dpt++] = dataraw[i];
+            lst = dataraw[i];
+            cntw = 1;
+        }
+        else cntw++;
+    }
+    w[dpt-1] = cntw;
+    
+    int lpt = 0, rpt = 0;
+    long long ans = 0;
+    while (rpt < dpt)
+    {
+        int flag = 0;
+        while (rpt < dpt && data[rpt] - data[lpt] > c)
+            lpt++, flag = 1;
+        while (rpt < dpt && data[rpt] - data[lpt] < c)
+            rpt++, flag = 2;
+        if (rpt < dpt && data[rpt] - data[lpt] == c)
+        {
+            ans += w[lpt] * w[rpt];
+            if (flag == 1) lpt++;
+            else if (flag == 2) rpt++;
+        }
+    }
+    cout<<ans;
+    //cin.get(),cin.get();
+    return 0;
+}
