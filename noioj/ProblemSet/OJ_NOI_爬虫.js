e4e53b42cbe68dda3f24303bde193b2b8a9ed9dc
北海若 <shiruoxi2010@163.com>
2018-09-08 19:48:30 +0800

    New Spider of NOIOJ (almost not working x)

--- noioj/ProblemSet/OJ_NOI_爬虫.js
+++ noioj/ProblemSet/OJ_NOI_爬虫.js
@@ -11,3 +11,23 @@ for (var i=1000;i<=1188;i++)
 			}
 		}
 	)
+
+    
+var st = "";
+var cnt = 0;
+for (var problem = 1001; problem < 1188; problem++)
+{
+	for (var data = 0; data < 20; data++)
+    {
+		$.get("http://oj.noi.cn/oj/index.php/main/download/"+problem+"/"+data+".in/1", function (pr, dt) {
+                return function (a) {
+                    cnt += 1;
+                    if (a.indexOf("PHP Error was encountered") == -1 && a.indexOf("You are not allowed to download this file!") == -1) {
+                        st += a + "#XCERETLE"+pr+"#"+dt+"#";
+                    }
+                    if (cnt == 187 * 20) console.log(st);
+                }
+            }(problem, data)
+        );
+    }
+}
